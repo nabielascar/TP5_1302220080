6952089a5b5f82eaf38966f275f5ad07e06c0a52
nabielascar <149862932+nabielascar@users.noreply.github.com>
2024-03-18 03:34:34 +0700

    merge done

--- TPMODUL5_1302220080/Program.cs
+++ TPMODUL5_1302220080/Program.cs
@@ -7,13 +7,42 @@ using System.Threading.Tasks;
 namespace TPMODUL5_1302220080
 {
     
+    class DataGeneric<T> {
+        T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T userName)
+        {
+            Console.WriteLine($"Halo user {userName}");
+        }
+    }
+
     class Program
     {
-
+        
 
         static void Main(string[] args)
         {
+            DataGeneric<string> stringData = new DataGeneric<string>("Y");
+            stringData.PrintData(); 
+            DataGeneric<string> nimData = new DataGeneric<string>("1302220080);
+            nimData.PrintData(); 
+           
+            HaloGeneric halo = new HaloGeneric();
+
             
+            string namaPanggilan = "Ascar";
+            halo.SapaUser(namaPanggilan);
+            Console.Read();
         }
     }
 }
